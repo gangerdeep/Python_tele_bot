448d31a9f96424aa83bac7f5ec95265fc4403884
SANDEEP <sandeepbhabhar00@gmail.com>
2026-03-22 21:55:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 TOKEN = "8283166443:AAHwuqWFuP9YhguBaUNA7ysItH1w8ModRKU"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Bot is live ✅")
+    await update.message.reply_text("Bot is Active 🤖✅")
 
 app = ApplicationBuilder().token(TOKEN).build()
 
